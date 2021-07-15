e5f98c3a282dd9633e68fffb67999bda348273c2
Victor Abrell <victor@abrell.se>
2021-07-15 14:51:27 +0200

    Release v1.3.5

---
+++ dist/types.d.ts
@@ -0,0 +1,22 @@
+export declare type OptionType = {
+    value: string | number;
+    label: string;
+};
+export declare type OptionsType = Array<OptionType>;
+export declare type Theme = {
+    headerBgColor?: string;
+    columnBorderColor?: string;
+    textColor?: string;
+    columnBgColor?: string;
+    buttonBgColor?: string;
+    buttonHoverBgColor?: string;
+    buttonDisabledBgColor?: string;
+    searchFocusBorderColor?: string;
+    optionHoverBgColor?: string;
+    optionSelectedBgColor?: string;
+};
+export declare type ColumnType = 'options' | 'selected';
+export declare type ActionTypes = 'add' | 'add-all' | 'remove' | 'remove-all' | 'move-up' | 'move-down';
+export declare type ActionMeta = {
+    action: ActionTypes;
+};
