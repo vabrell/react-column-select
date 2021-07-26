f5dd5b0a92eda7f75f745365e5e9d11431012196
Victor Abrell <victor@abrell.se>
2021-07-26 12:44:00 +0200

    For release

--- examples/data/options.ts
+++
@@ -1,8 +0,0 @@
-export const chessOptions = [
-  { value: 'pawn', label: 'Pawn' },
-  { value: 'bishop', label: 'Bishop' },
-  { value: 'knight', label: 'Knight' },
-  { value: 'rook', label: 'Rook' },
-  { value: 'queen', label: 'Queen' },
-  { value: 'king', label: 'King' },
-]
